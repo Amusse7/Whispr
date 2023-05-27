f1f20e1c474f78d2a1dfd9e187f63f50ab944403
Abdulkadir Musse <72959571+Amusse7@users.noreply.github.com>
2023-05-26 23:21:41 -0400

    Changes

---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import { styled } from "@mui/system";
+
+const FlexBetween = Styled(Box)({
+    display: "flex",
+    justifyContent: "space-between",
+    alignitems: "center"
+})
+
+export default FlexBetween;
